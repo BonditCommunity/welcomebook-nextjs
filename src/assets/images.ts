dcd19265e4105059e75539f0e0465fd72e3943ad
smk950226 <smk950226@naver.com>
2024-07-12 17:10:20 +0900

    change img

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -4,12 +4,12 @@ const basePath = '/assets/images';
 
 export const imgError: Asset = {
     src: `${basePath}/img_error.png`,
-    width: 2800,
-    height: 2800,
+    width: 900,
+    height: 900,
 };
 
 export const imgSuccess: Asset = {
     src: `${basePath}/img_success.png`,
-    width: 2800,
-    height: 2800,
+    width: 900,
+    height: 900,
 };
