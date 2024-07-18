af9cd23125d7c2d0d79a35626b6ff2b43fe812d0
smk950226 <smk950226@naver.com>
2024-07-18 11:16:06 +0900

    login redirect

--- src/contexts/authentication/provider.tsx
+++ src/contexts/authentication/provider.tsx
@@ -1,19 +1,26 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { onAuthStateChanged, signInAnonymously, User } from 'firebase/auth';
 
 import { ChildrenProps } from '@/@types';
 import { AuthContext } from './context';
 import { firebase } from '@/firebase';
+import { routes } from '@/routes';
 
 export const AuthProvider: React.FC<ChildrenProps> = ({ children }) => {
+    const router = useRouter();
+
     const [user, setUser] = useState<User>();
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(firebase, async user => {
             if (user) {
+                if (!user.isAnonymous) {
+                    router.replace(routes.wishlist);
+                }
                 setUser(user);
             } else {
                 const credential = await signInAnonymously(firebase);
