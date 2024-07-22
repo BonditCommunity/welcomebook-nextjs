7fea4c988a3107c0e2006f6333278673c23752ae
배상훈 <sanghoonpai@baesanghun-ui-MacBookPro.local>
2024-07-22 20:55:34 +0900

    privacy and service

--- src/screens/terms/privacy/index.tsx
+++ src/screens/terms/privacy/index.tsx
@@ -27,6 +27,24 @@ export function Privacy() {
                 }}>
                 Privacy Policy
             </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                This Policy explains the way of treatment of the information which is provided or collected in the web sites on which this Policy is posted. In addition the Policy also explains the information which is provided or collected in the course of using the applications of the Company which exist in the websites or platforms of other company.
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                The Company is the controller of the information provided or collected in the websites on which this Policy is posted and in the course of using the applications of the Company which exist in the websites or platforms of other company. Through this Policy, the Company regards personal information of the users as important and inform them of the purpose and method of Company&apos;s using the personal information provided by the users and the measures taken by the Company for protection of those personal information
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                (1) Personal information items to be collected Personal information items to be collected by the Company are as follows: Name, address, telephone number, and email address ∘ Payment information including account number and card number ∘ Delivery information including delivery address, name and contact information of recipient ∘ Information of place of taking pictures and date of creation of files ∘ Information of service use of members such as the type of contents watched or used by members, the persons interacting or sharing contents with members, frequencies and period of activities of members.
+            </IBody2>
         </Sheet>
     );
 }
--- src/screens/terms/service/index.tsx
+++ src/screens/terms/service/index.tsx
@@ -27,6 +27,43 @@ export function Service() {
                 }}>
                 Terms of Service
             </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                All letters and photos will be made public at the school start date.
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                Funding and the use of funds are available until 15 days after the school start date.
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                The funded amount is non-refundable under any circumstances.
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                10%-15% of the total funded amount will be used as a donation for school education development.
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                Once a letter or photo is uploaded, it cannot be modified or deleted.
+            </IBody2>
+            <IBody2
+                style={{
+                    marginTop: 15,
+                }}>
+                After the school starts, If the funding is not used within 15 days, the remaining amount will be used for donation.
+            </IBody2>
         </Sheet>
     );
+
 }
